4636dafc331a7d7d4741ac83635e5462345201ff
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-17 18:23:35 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -10,21 +10,22 @@ user_name = os.getenv("BROWSERSTACK_USERNAME")
 access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
 build_name = os.environ.get("JENKINS_LABEL", "0")
 
-desired_cap = [{
+desired_cap = [
+{
     "platformName" : "android",
     "platformVersion" : "9.0",
     "deviceName" : "Google Pixel 7 Pro",
     "app" : "bs://f9529d2e64d5fb2183d28680ff7e3af0b4a2ea47",
     'build': build_name
 },
-    desired_cap = {
+{
     "platformName" : "android",
     "platformVersion" : "13.0",
     "deviceName" : "Samsung Galaxy S23 Ultra",
     "app" : "bs://f9529d2e64d5fb2183d28680ff7e3af0b4a2ea47",
     'build': build_name
 },
-    desired_cap = {
+{
     "platformName" : "android",
     "platformVersion" : "11.0",
     "deviceName" : "OnePlus 9",
