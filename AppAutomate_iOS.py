5c284e1c0ef72045083d97bf2f583221ebc20f91
pragya-28 <39649873+pragya-28@users.noreply.github.com>
2023-05-19 14:29:27 +0530

    Update AppAutomate_iOS.py

--- AppAutomate_iOS.py
+++ AppAutomate_iOS.py
@@ -1,4 +1,3 @@
-import selenium
 import os
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
@@ -11,7 +10,26 @@ user_name = os.getenv("BROWSERSTACK_USERNAME")
 access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
 build_name = os.environ.get("JENKINS_LABEL", "0")
 
+desired_cap = {
+    "platformName" : "android",
+    "platformVersion" : "16.0",
+    "deviceName" : "iPhone 14",
+    "app" : "bs://a3129b2292fe6e8544f151efa019555ab058ac97",
+    'build': build_name
+}
 
-driver = webdriver.Remote("https://"+user_name+":"+access_key+"@hub-cloud.browserstack.com/wd/hub")
+driver = webdriver.Remote("https://"+user_name+":"+access_key+"@hub-cloud.browserstack.com/wd/hub", desired_cap)
 
+search_element = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
+)
+search_element.click()
+search_input = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable(
+        (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text"))
+)
+search_input.send_keys("BrowserStack")
+time.sleep(5)
+search_results = driver.find_elements(AppiumBy.CLASS_NAME, "android.widget.TextView")
+assert (len(search_results) > 0)
 driver.quit()
